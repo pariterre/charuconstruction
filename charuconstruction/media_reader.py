Fixed computation of error and fix translation factor

charuconstruction/media_reader.py
@@ -6,7 +6,7 @@ import numpy as np
 
 from .camera import Camera
 from .frame import Frame
-from .math import Transformation
+from .math import Transformation, TranslationVector, RotationMatrix, Vector3
 
 if TYPE_CHECKING:
     from .charuco import Charuco
@@ -80,44 +80,120 @@ class CharucoMockReader(MediaReader):
         camera: Camera,
         transformations: dict[Charuco, Iterable[Transformation]] = None,
     ):
+        """
+        A mock reader that simulates a video feed of Charuco boards with given transformations.
+        If no transformations are provided, a controller window is shown to manually adjust the
+        pose of the boards.
+        """
         self._boards = boards
-        if transformations is None:
-            transformations = {
-                charuco: [Transformation()] for charuco in boards
-            }
 
         # Sanity checks for angles
-        self._frame_count = (
-            len(transformations[next(iter(transformations))])
-            if transformations
-            else 0
-        )
-        for charuco in transformations.keys():
-            if len(transformations[charuco]) != self._frame_count:
-                raise ValueError(
-                    "Number of transformations must match number of boards in all frames."
-                )
+        if transformations is None:
+            self._frame_count = -1
+        else:
+            self._frame_count = (
+                len(transformations[next(iter(transformations))])
+                if transformations
+                else 0
+            )
+            for charuco in transformations.keys():
+                if len(transformations[charuco]) != self._frame_count:
+                    raise ValueError(
+                        "Number of transformations must match number of boards in all frames."
+                    )
         self._transformations = transformations
 
         self._camera = camera
+        self._dynamic_frame_ready = False
         self._current_index = 0
 
         super().__init__()
 
     def destroy(self):
-        pass
+        if self.with_gui:
+            for board in self._boards:
+                ctrl = f"Controle {board}"
+                cv2.destroyWindow(ctrl)
 
     @property
-    def transformations(self) -> dict[Charuco, tuple[Transformation]]:
-        return self._transformations
+    def with_gui(self) -> bool:
+        return self._frame_count < 0
+
+    @property
+    def frame_count(self) -> int:
+        return self._frame_count
+
+    def transformations(self, charuco: Charuco) -> Transformation:
+        """
+        Return the current transformation for a given Charuco board.
+        """
+        if self.with_gui:
+            ctrl = f"Controle {charuco}"
+            return Transformation(
+                translation=TranslationVector(
+                    x=cv2.getTrackbarPos("Trans X", ctrl) / 100,
+                    y=cv2.getTrackbarPos("Trans Y", ctrl) / 100,
+                    z=cv2.getTrackbarPos("Trans Z", ctrl) / 100,
+                ),
+                rotation=RotationMatrix.from_euler(
+                    Vector3(
+                        x=cv2.getTrackbarPos("Rot X", ctrl),
+                        y=cv2.getTrackbarPos("Rot Y", ctrl),
+                        z=cv2.getTrackbarPos("Rot Z", ctrl),
+                    ),
+                    sequence=RotationMatrix.Sequence.ZYX,
+                    degrees=True,
+                ),
+            )
+        else:
+            return self._transformations[charuco][self._current_index - 1]
 
     def __iter__(self) -> "CharucoMockReader":
+        if self.with_gui:
+            for board in self._boards:
+                ctrl = f"Controle {board}"
+                cv2.namedWindow(ctrl, cv2.WINDOW_NORMAL)
+                cv2.resizeWindow(ctrl, 400, 300)
+
+                cv2.createTrackbar("Trans X", ctrl, 0, 1000, self._set_ready)
+                cv2.setTrackbarMin("Trans X", ctrl, -500)
+                cv2.setTrackbarMax("Trans X", ctrl, 500)
+                cv2.createTrackbar("Trans Y", ctrl, 0, 1000, self._set_ready)
+                cv2.setTrackbarMin("Trans Y", ctrl, -500)
+                cv2.setTrackbarMax("Trans Y", ctrl, 500)
+                cv2.createTrackbar("Trans Z", ctrl, 150, 500, self._set_ready)
+                cv2.setTrackbarMin("Trans Z", ctrl, 0)
+                cv2.setTrackbarMax("Trans Z", ctrl, 500)
+                cv2.createTrackbar("Rot X", ctrl, 0, 360, self._set_ready)
+                cv2.setTrackbarMin("Rot X", ctrl, -360)
+                cv2.setTrackbarMax("Rot X", ctrl, 360)
+                cv2.createTrackbar("Rot Y", ctrl, 0, 360, self._set_ready)
+                cv2.setTrackbarMin("Rot Y", ctrl, -360)
+                cv2.setTrackbarMax("Rot Y", ctrl, 360)
+                cv2.createTrackbar("Rot Z", ctrl, 0, 360, self._set_ready)
+                cv2.setTrackbarMin("Rot Z", ctrl, -360)
+                cv2.setTrackbarMax("Rot Z", ctrl, 360)
+
+                self._dynamic_frame_ready = True
+
         self._current_index = 0
         return self
 
+    def _set_ready(self, _) -> bool:
+        self._dynamic_frame_ready = True
+
     def _read_frame(self):
-        if self._current_index >= self._frame_count:
-            return None
+        if self.with_gui:
+            while not self._dynamic_frame_ready:
+                cv2.waitKey(10)
+                if cv2.getWindowProperty("frame", cv2.WND_PROP_VISIBLE) == 0:
+                    return None
+            self._dynamic_frame_ready = False
+        else:
+            if self._current_index >= self._frame_count:
+                return None
+            # We increment now since the self.transformations uses the current_index - 1
+            self._current_index += 1
 
         # First create a cv2 image with a white background which corresponds to a
         # distant wall
@@ -133,17 +209,14 @@ class CharucoMockReader(MediaReader):
 
         # Move the image further away and rotate the boards and get their images
         for board in self._boards:
-            transformations = self._transformations[board][self._current_index]
             img = self._project_board(
                 board.cv2_board_image,
-                transformation=transformations,
+                transformation=self.transformations(charuco=board),
             )
             projected_img = cv2.cvtColor(src=img, code=cv2.COLOR_GRAY2BGR)
 
             masked = projected_img < 255
             frame[masked] = projected_img[masked]
-
-        self._current_index += 1
 
         # Encode and decode to get a proper Frame object
         success, buf = cv2.imencode(".png", frame)
@@ -164,27 +237,24 @@ class CharucoMockReader(MediaReader):
             transformation (Transformation): The transformation containing translation and rotation.
         """
 
-        # For a planar homography: H = K @ (R - t*n^T/d) @ K^(-1)
-        # where n is the normal to the plane (0,0,1) and d is distance
-        # Since the board is in the z=0 plane initially, we use:
-        # H = K @ (R - t @ [0,0,1]) @ K^(-1)
-        normal = np.array([[0.0], [0.0], [1.0]])
-        d = 1.0
-        d0 = np.array([[0.0], [0.0], [1]])
+        # Corners of the img
+        h, w = img.shape[:2]
+        s_matrix = np.array(
+            [[1, 0, -w / 2], [0, 1, -h / 2], [0, 0, 1]], dtype=np.float32
+        )
 
-        translation = transformation.translation.vector
+        translation = (
+            transformation.translation.vector * self._camera.focal_length
+        )
         rotation = transformation.rotation.matrix
+        rt_matrix = np.hstack((rotation, translation))
+        h_matrix = self._camera.matrix @ np.delete(rt_matrix, 2, 1)
+        h_final = h_matrix @ s_matrix
+
         return cv2.warpPerspective(
             img,
-            (
-                self._camera.matrix
-                @ (rotation - (translation + d0) @ normal.T / d)
-                @ np.linalg.inv(self._camera.matrix)
-            ),
-            (
-                int(self._camera.sensor_width),
-                int(self._camera.sensor_height),
-            ),
+            h_final,
+            (int(self._camera.sensor_width), int(self._camera.sensor_height)),
             flags=cv2.INTER_LINEAR,
             borderValue=255,
         )

run/main.py
@@ -17,6 +17,7 @@ import numpy as np
 def simulate_reader(
     camera: Camera,
     charuco_boards: list[Charuco],
+    use_gui: bool = False,
 ) -> CharucoMockReader:
     frame_count = 100
 
@@ -24,23 +25,26 @@ def simulate_reader(
     transformations: dict[Charuco, list[Transformation]] = {
         charuco: [] for charuco in charuco_boards
     }
-    for i in range(frame_count):
-        for j in range(len(charuco_boards)):
-            base_translation = -0.4 if j == 0 else -0.7
-            translation = i * 0.02 * (1 if j == 0 else -1)
-            rotation = i * 1 * (1 if j == 0 else -1)
-            transformations[charuco_boards[j]].append(
-                Transformation(
-                    translation=TranslationVector(
-                        base_translation + translation, -0.2 - translation, -1.5
-                    ),
-                    rotation=RotationMatrix.from_euler(
-                        Vector3(rotation, rotation, rotation),
-                        degrees=True,
-                        sequence=RotationMatrix.Sequence.ZYX,
-                    ),
+    if use_gui:
+        transformations = None
+    else:
+        for i in range(frame_count):
+            for j in range(len(charuco_boards)):
+                base_translation = -0.7 if j == 0 else 0.7
+                translation = i * 0.01 * (1 if j == 0 else -1) * 0
+                rotation = i * 1 * (1 if j == 0 else -1)
+                transformations[charuco_boards[j]].append(
+                    Transformation(
+                        translation=TranslationVector(
+                            base_translation + translation, 0, 2.5
+                        ),
+                        rotation=RotationMatrix.from_euler(
+                            Vector3(rotation, rotation, rotation),
+                            degrees=True,
+                            sequence=RotationMatrix.Sequence.ZYX,
+                        ),
+                    )
                 )
-            )
 
     # Detect markers for each Charuco board at each frame
     return CharucoMockReader(
@@ -58,14 +62,16 @@ def main():
         charuco_boards.append(Charuco.load(Path(folder_name)))
 
     # Simulate a reader (since we don't have real media for now)
-    reader = simulate_reader(camera, charuco_boards)
+    reader = simulate_reader(
+        camera, charuco_boards, use_gui=os.environ.get("WITH_GUI") == "true"
+    )
 
     should_continue = True
     initial_guess = {}
     errors: dict[Charuco, list[np.ndarray]] = {}
-    for frame_index, frame in enumerate(reader):
+    for frame in reader:
         frame_to_draw = Frame(frame.get())
-        for charuco_index, charuco in enumerate(charuco_boards):
+        for charuco in charuco_boards:
             frame_to_draw, results = charuco.detect(
                 frame_to_draw,
                 initial_guess=initial_guess.get(charuco),
@@ -76,7 +82,7 @@ def main():
             initial_guess[charuco] = results
 
             # Compute the reconstruction error in degrees
-            true_transformation = reader.transformations[charuco][frame_index]
+            true_transformation = reader.transformations(charuco)
             true_values = true_transformation.rotation.to_euler(
                 sequence=RotationMatrix.Sequence.ZYX, degrees=True
             ).as_array()
@@ -91,11 +97,13 @@ def main():
                 errors[charuco] = []
             errors[charuco].append(true_values - reconstructed_values)
 
-        should_continue = frame_to_draw.show(wait_time=None)
+        should_continue = frame_to_draw.show(
+            wait_time=1 if reader.with_gui else None
+        )
         if not should_continue:
             break
 
-    if should_continue:
+    if should_continue and not reader.with_gui:
         frame_to_draw.show(wait_time=None)
 
     reader.destroy()
@@ -103,6 +111,8 @@ def main():
     # Print the mean error for each Charuco board
     for charuco, error_list in errors.items():
         error_array = np.array(error_list).squeeze()
+        if not np.isfinite(error_array).any():
+            continue
         mean_error = np.nanmean(np.abs(error_array), axis=0)
         std_error = np.nanstd(error_array, axis=0)
         print(